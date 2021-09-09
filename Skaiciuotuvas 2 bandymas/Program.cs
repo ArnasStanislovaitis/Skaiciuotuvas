c12f43828d4117522f5f57ec712f3f3dc165afd2
ArnasStanislovaitis <89936719+ArnasStanislovaitis@users.noreply.github.com>
2021-09-09 08:51:53 +0300

    Papildytas skaiciuotuvas
    Pridetas while ciklas main'e

--- Skaiciuotuvas 2 bandymas/Program.cs
+++ Skaiciuotuvas 2 bandymas/Program.cs
@@ -6,7 +6,36 @@ namespace GitHub_skaiciuotuvas
     {
         static void Main(string[] args)
         {
-            
+            while (true)
+            {
+                Console.WriteLine("\nIveskite pasirinkima \n1.Lyginiu skaiciu spausdimas \n2.Nelyginiu skaiciu spausdimas\n3. Sudetis \n4.Atimtis \n5.Dalyba \n6.Daugyba");
+                int Pasirinkimas = int.Parse(Console.ReadLine());
+                if (Pasirinkimas == 1)
+                {
+                    LyginiuMetodas();
+                }
+                if (Pasirinkimas == 2)
+                {
+                    NelyginiuMetodas();
+                }
+                if (Pasirinkimas == 3)
+                {
+                    Sudetis();
+                }
+                if (Pasirinkimas == 4)
+                {
+                    Atimtis();
+                }
+                if (Pasirinkimas == 5)
+                {
+                    Dalyba();
+                }
+                if (Pasirinkimas == 6)
+                {
+                    Daugyba();
+                }
+            }
+
 
         }
         static void LyginiuMetodas()
@@ -32,9 +61,9 @@ namespace GitHub_skaiciuotuvas
         static int Sudetis()
         {
             Console.WriteLine("Iveskite pirma skaiciu");
-            int Pirmas = int.Parse(Console.WriteLine());
+            int Pirmas = int.Parse(Console.ReadLine());            
             Console.WriteLine("Iveskite Antra skaiciu");
-            int Antras = int.Parse(Console.WriteLine());
+            int Antras = int.Parse(Console.ReadLine());
             int Suma = Pirmas + Antras;
             Console.WriteLine(Suma);
             return Suma;
@@ -42,9 +71,9 @@ namespace GitHub_skaiciuotuvas
         static int Atimtis()
         {
             Console.WriteLine("Iveskite pirma skaiciu");
-            int Pirmas = int.Parse(Console.WriteLine());
+            int Pirmas = int.Parse(Console.ReadLine());
             Console.WriteLine("Iveskite Antra skaiciu");
-            int Antras = int.Parse(Console.WriteLine());
+            int Antras = int.Parse(Console.ReadLine());
             int Rezultatas = Pirmas - Antras;
             Console.WriteLine(Rezultatas);
             return Rezultatas;
@@ -52,19 +81,19 @@ namespace GitHub_skaiciuotuvas
         static int Daugyba()
         {
             Console.WriteLine("Iveskite pirma skaiciu");
-            int Pirmas = int.Parse(Console.WriteLine());
+            int Pirmas = int.Parse(Console.ReadLine());
             Console.WriteLine("Iveskite Antra skaiciu");
-            int Antras = int.Parse(Console.WriteLine());
+            int Antras = int.Parse(Console.ReadLine());
             int Rezultatas = Pirmas * Antras;
             Console.WriteLine(Rezultatas);
             return Rezultatas;
         }
-        static double Daugyba()
+        static double Dalyba()
         {
             Console.WriteLine("Iveskite pirma skaiciu");
-            double Pirmas = double.Parse(Console.WriteLine());
+            double Pirmas = double.Parse(Console.ReadLine());
             Console.WriteLine("Iveskite Antra skaiciu");
-            double Antras = double.Parse(Console.WriteLine());
+            double Antras = double.Parse(Console.ReadLine());
             double Rezultatas = Pirmas / Antras;
             Console.WriteLine(Rezultatas);
             return Rezultatas;
